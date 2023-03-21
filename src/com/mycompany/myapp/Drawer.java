4e721c1102431f84c13d69608bf0bf95c236418f
Mourad Aouini <mouradaouini@hotmail.fr>
2023-03-21 03:20:10 +0100

    Drawer design update

--- src/com/mycompany/myapp/Drawer.java
+++ src/com/mycompany/myapp/Drawer.java
@@ -16,7 +16,8 @@ public class Drawer {
     
     private Form form;
     private boolean sideMenuOpen;
-    
+    FontImage edit = FontImage.createMaterial(FontImage.MATERIAL_MODE_EDIT, "MyIcon", 6);
+    FontImage nav = FontImage.createMaterial(FontImage.MATERIAL_MENU, "Navigation", 6);
     public Drawer() {
         form = new Form(new BorderLayout());
         Toolbar tb = new Toolbar();
@@ -36,29 +37,35 @@ public class Drawer {
             }
         });
         tb.addComponentToSideMenu(menuButton);
+        Container topsideMenuContainer = new Container(new BoxLayout(BoxLayout.Y_AXIS_BOTTOM_LAST));
         
         Container sideMenuContainer = new Container(new BoxLayout(BoxLayout.Y_AXIS_BOTTOM_LAST));
         sideMenuContainer.setUIID("SideMenu");
         
         Label title = new Label("G-store Esprit");
-        title.setUIID("SideMenuTitle");
+        title.setUIID("label1");
         Container titleContainer = new Container(new BorderLayout());
-        titleContainer.getAllStyles().setBgColor(1);
+       
         titleContainer.add(BorderLayout.CENTER, title);
-        sideMenuContainer.add(titleContainer);
+        topsideMenuContainer.add(titleContainer);
         
         Button modifierProfileButton = new Button("Modifier Profile");
+        modifierProfileButton.setIcon(edit);
+        modifierProfileButton.setUIID("text");
         modifierProfileButton.addActionListener(e -> {
             // Handle modifier profile button click
         });
         sideMenuContainer.add(modifierProfileButton);
         
         Button navigationLibreButton = new Button("Navigation Libre");
+        navigationLibreButton.setIcon(nav);
+        navigationLibreButton.setUIID("text");
         navigationLibreButton.addActionListener(e -> {
             // Handle navigation libre button click
         });
         sideMenuContainer.add(navigationLibreButton);
         
+        tb.addComponentToSideMenu(topsideMenuContainer);
         tb.addComponentToSideMenu(sideMenuContainer);
         
         Command exitCommand = new Command("Exit") {
@@ -74,8 +81,4 @@ public class Drawer {
         return form;
     }
 }
-
-
-
-
 
